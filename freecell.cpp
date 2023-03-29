b3b2d0d9396a47226082bca63d585437edb3d198
Maurycy Szmuc <mauryc_s@wp.pl>
2023-03-29 21:07:38 +0200

    Utworzono freecell.cpp, dodatkowe funkcje talli

---
+++ freecell.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <random>
+/*
+KODOWANIE KART:
+s - serce   1 - As  
+t - trefl   1..10 - odpowiednio liczby
+k - karo    11 - jopek
+p - pik     12 - dama
+            13 - król
+*/
+const std::vector<char> colors = {'s','t','k','p'};
+
+class PlayingCards{
+    public:
+        std::vector<std::string> deck;
+        PlayingCards(){
+            for (auto c :colors)
+            {
+                for (int i = 1; i <= 13; i++)
+                {
+                    deck.push_back(c + std::to_string(i));
+                }
+            }
+        }
+        void shuffleCards(){
+            std::random_device rd;
+            std::mt19937 g(rd());
+            std::shuffle(deck.begin(),deck.end(),g);
+        }
+};
+int main(){
+    PlayingCards *cards = new PlayingCards;
+    std::thread t1(&PlayingCards::shuffleCards,cards);
+    for(auto i : cards->deck){
+        std::cout<<i<<'\n';
+    }
+    t1.join();
+    std::cout<<'\n';
+    for(auto i : cards->deck){
+        std::cout<<i<<'\n';
+    }
+    system("pause");
+}
