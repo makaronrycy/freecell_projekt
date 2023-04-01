14931ca1b314585a1cce35f752886e005ed6d9b9
Maurycy Szmuc <mauryc_s@wp.pl>
2023-04-01 20:40:40 +0200

    utworzono header.h

---
+++ header.h
@@ -0,0 +1,18 @@
+//Bibilioteki
+#include <iostream>
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <random>
+#include <iomanip>
+//najczęściej używane obiekty z std
+using std::vector;
+using std::cout;
+using std::cin;
+using std::thread;
+using std::string;
+
+
+ 
